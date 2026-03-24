8eca351c1a744dd46d82a86d2ae2001ac7378fac
rawcam <rawcam@ya.ru>
2026-03-24 16:09:03 +0300

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,1 +1,12 @@
-
+// tracts.js (или led.js, sound.js и т.д.)
+const VideoModule = (function() {
+    let unsubscribe = null;
+    function init() {
+        console.log('TractsModule stub');
+        unsubscribe = AppState.subscribe(()=>{});
+    }
+    function destroy() {
+        if (unsubscribe) unsubscribe();
+    }
+    return { init, destroy };
+})();
